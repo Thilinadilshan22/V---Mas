8361e716526fc80bc22ffcc1c01771d8e698a65a
Thilina Dilshan <thilinadilshan626@gmail.com>
2026-03-20 23:15:33 +0530

    chore: Install frontend dependencies for the EMS frontend application.

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -5,17 +5,19 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import DashboardPage from './pages/DashboardPage'
 import UsersPage from './pages/UsersPage'
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login"     element={<LoginPage />} />
+          <Route path="/register"  element={<RegisterPage />} />
           <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
-          <Route path="/users" element={<PrivateRoute><UsersPage /></PrivateRoute>} />
-          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/users"     element={<PrivateRoute><UsersPage /></PrivateRoute>} />
+          <Route path="/profile"   element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
+          <Route path="/"          element={<Navigate to="/login" replace />} />
         </Routes>
       </Router>
     </AuthProvider>
